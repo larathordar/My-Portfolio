29572611b16cd2db01c0ca2345d652fd95d5fe03
Lara Thordardottir <lara-thordar@hotmail.com>
2018-02-11 13:38:52 +0100

    tests passing

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,21 +1,31 @@
-describe 'Index Page', type: :feature do
-  it 'displays project list' do
-    visit '/'
+describe 'about_me', type: :feature do
+  it 'displays work experience list' do
+    visit 'about_me.html'
 
-    expect(page).to have_css '.projects'
-    within '.projects' do
-      expect(page).to have_content 'My First Website'
-      expect(page).to have_content 'FizzBuzz'
+    expect(page).to have_css '#workExperience'
+    within '#workExperience' do
+      expect(page).to have_content 'Feb 2016 - Aug 2017'
+      expect(page).to have_content 'Summer 2015'
+      expect(page).to have_content 'Summer 2015'
+      expect(page).to have_content 'Gardener, Orkuveita Reykjavíkur, Iceland'
+      expect(page).to have_content 'Nursery school teacher, Leikskólinn Furuborg, Iceland'
+      expect(page).to have_content 'House keeping, Hlemmmur Square, Iceland.'
+      expect(page).to have_content '-----------------------------------'
     end
   end
 
-  it 'renders footer partial' do
-  visit '/'
+  it 'displays education list' do
+    visit 'about_me.html'
 
-  expect(page).to have_selector 'footer'
-  within 'footer' do
-    expect(page).to have_content 'My Portfolio'
-    expect(page).to have_content 'Built using the awesome Middleman framework'
+    expect(page).to have_css '#education'
+    within '#education' do
+      expect(page).to have_content 'Current, Craft Academy,'
+      expect(page).to have_content '2014- 2017 University of Iceland,'
+      expect(page).to have_content '2015 Oxford Brookes University,'
+      expect(page).to have_content '2007-2011 Kvennaskólinn í Reykjavík,'
+      expect(page).to have_content '2005-2007 Árbæjarskóli'
+      expect(page).to have_content '-----------------------------------'
+    end
   end
-  end
+
 end
